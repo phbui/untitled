2b10ede3882ff07a938c6dfb52f4191bcfe48ff5
pbuieifu <pbui@eifu.com>
2024-07-18 14:08:12 -0400

    preview improvements

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -6,20 +6,22 @@ import { Editor_Type } from "../pages/Editor";
 
 const Preview: React.FC = () => {
   const editor = useContext(Editor_Type);
-  const [render, setRender] = useState<boolean>(false);
-  const [scene, setScene] = useState<Scene>();
-  const [dialogue, setDialogue] = useState<Dialogue>();
-  const [npc, setNPC] = useState<Game_Character>();
+  const [scene, setScene] = useState<Scene | undefined>();
+  const [dialogue, setDialogue] = useState<Dialogue | undefined>();
+  const [npc, setNPC] = useState<Game_Character | undefined>();
 
   const getNPC = (id: string) => characters[id];
 
   const getChapter = (id: string) => (editor.story as Story)[id];
 
-  const getScene = (id: string) =>
-    getChapter(editor.currentChapterId).scenes[id];
+  const getScene = (chapterId: string, sceneId: string) =>
+    getChapter(chapterId).scenes[sceneId];
 
-  const getDialogue = (id: string) =>
-    getScene(editor.currentSceneId).dialogue[id];
+  const getDialogue = (
+    chapterId: string,
+    sceneId: string,
+    dialogueId: string
+  ) => getScene(chapterId, sceneId).dialogue[dialogueId];
 
   const summonOptions = (dialogueOptions: Dialogue_Option[]) => {
     return dialogueOptions.map((option: Dialogue_Option) => (
@@ -31,50 +33,57 @@ const Preview: React.FC = () => {
   };
 
   useEffect(() => {
-    setRender(
-      editor.currentChapterId?.length > 0 &&
-        editor.currentSceneId?.length > 0 &&
-        editor.currentDialogueId?.length > 0
-    );
+    if (
+      editor.currentChapterId &&
+      editor.currentSceneId &&
+      editor.currentDialogueId
+    ) {
+      const currentScene = getScene(
+        editor.currentChapterId,
+        editor.currentSceneId
+      );
+      const currentDialogue = getDialogue(
+        editor.currentChapterId,
+        editor.currentSceneId,
+        editor.currentDialogueId
+      );
+      setScene(currentScene);
+      setDialogue(currentDialogue);
+      setNPC(getNPC(currentDialogue.character_id));
+    }
   }, [
     editor.currentChapterId,
     editor.currentSceneId,
     editor.currentDialogueId,
   ]);
 
-  useEffect(() => {
-    if (render) {
-      setScene(getScene(editor.currentSceneId));
-      setDialogue(getDialogue(editor.currentDialogueId));
-      setNPC(getNPC(getDialogue(editor.currentDialogueId).character_id));
-    }
-  }, [render, editor.currentDialogueId]);
+  if (!scene || !dialogue) {
+    return null;
+  }
 
   return (
-    render && (
-      <div className="game">
-        {scene?.background && (
-          <img className="game-background" src={scene?.background} />
-        )}
-        <div className="game-buttons"></div>
-        <div className="game-characters">
-          <div className={`pc`}></div>
-          {npc && (
-            <div className="npc" key={dialogue?.character_id}>
-              <img src={npc.url} />
-            </div>
-          )}
-        </div>
-        <div className="dialogue-container">
-          <div>{npc?.name}</div>
-          <Typewriter dialogue={dialogue} getNext={() => {}} />
-          <div className="dialogue-options">
-            {dialogue?.next.dialog_options &&
-              summonOptions(dialogue?.next.dialog_options)}
+    <div className="game">
+      {scene.background && (
+        <img className="game-background" src={scene.background} />
+      )}
+      <div className="game-buttons"></div>
+      <div className="game-characters">
+        <div className="pc"></div>
+        {npc && (
+          <div className="npc" key={dialogue.character_id}>
+            <img src={npc.url} alt={npc.name} />
           </div>
+        )}
+      </div>
+      <div className="dialogue-container">
+        <div>{npc?.name}</div>
+        <Typewriter dialogue={dialogue} getNext={() => {}} />
+        <div className="dialogue-options">
+          {dialogue.next.dialog_options &&
+            summonOptions(dialogue.next.dialog_options)}
         </div>
       </div>
-    )
+    </div>
   );
 };
 
